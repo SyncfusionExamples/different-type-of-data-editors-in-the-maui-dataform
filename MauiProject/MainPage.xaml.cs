7197b92e4846fdfed1a6e392411582bd4b0a384e
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-07-17 15:28:51 +0530

    Adding the MAUI DataForm editor sample
    Adding the MAUI DataForm editor sample

---
+++ MauiProject/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Syncfusion.Maui.DataForm;
+
+namespace MauiProject;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        dataForm.ItemsSourceProvider = new ItemSourceProviderExt();
+        //dataForm.GenerateDataFormItem += OnGenerateDataFormItem;
+        //dataForm.RegisterEditor("Country", DataFormEditorType.ComboBox);
+    }
+
+    //private void OnGenerateDataFormItem(object sender, GenerateDataFormItemEventArgs e)
+    //{
+    //    if (e.DataFormItem != null && e.DataFormItem.FieldName == "Country" && e.DataFormItem is DataFormComboBoxItem comboBoxItem)
+    //    {
+    //        List<string> list = new List<string>() {
+    //                "Indonesia",
+    //                "Italy",
+    //                "India",
+    //                "Iran",
+    //                "Iraq",
+    //                "Uganda",
+    //                "Ukraine",
+    //                "Canada",
+    //                "Australia",
+    //                "Uzbekistan",
+    //                "France",
+    //                "United Kingdom",
+    //                "United States"
+    //            };
+    //        comboBoxItem.ItemsSource = list;
+    //    }
+    //}
+
+    public class ItemSourceProviderExt : IDataFormSourceProvider
+    {
+        public object GetSource(string sourceName)
+        {
+            if (sourceName == "Country")
+            {
+                List<string> list = new List<string>() {
+                    "Indonesia",
+                    "Italy",
+                    "India",
+                    "Iran",
+                    "Iraq",
+                    "Uganda",
+                    "Ukraine",
+                    "Canada",
+                    "Australia",
+                    "Uzbekistan",
+                    "France",
+                    "United Kingdom",
+                    "United States"
+                };
+                return list;
+            }
+            return new List<string>();
+        }
+    }
+}
+
+
